8167948ad67dfc50944b009f8199f464cd97fec2
Vitaly <vitalyasatka@gmail.com>
2023-04-18 02:06:50 +0500

    feat: add solutions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let repeatTimes = options.hasOwnProperty('repeatTimes') ? options.repeatTimes : 1,
+    separator = options.hasOwnProperty('separator') ? options.separator : '+',
+    addition = options.hasOwnProperty('addition') ? options.addition : '',
+    additionRepeatTimes = options.hasOwnProperty('additionRepeatTimes') ? options.additionRepeatTimes : 1,
+    additionSeparator = options.hasOwnProperty('additionSeparator') ? options.additionSeparator : '|',
+    result = '';
+  
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      result += addition;
+      if (j !== additionRepeatTimes - 1) {
+        result += additionSeparator;
+      }
+    }
+
+    if (i !== repeatTimes - 1) {
+      result += separator;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(first=true) {
+    if (first === false) {
+      this.reverse = true;
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    key = key.repeat(Math.ceil(message.length / key.length));
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) > 64 && message.charCodeAt(i) < 91) {
+        let newCharCode = ((message.charCodeAt(i) + key.charCodeAt(keyIndex)) % 26) + 65;
+        result += String.fromCharCode(newCharCode);
+        keyIndex++;
+      }
+      else {
+        result += message[i];
+      }
+    }
+    if (this.reverse) {
+      result = result.split('').reverse().join('');
+    }
+    return result;
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    key = key.repeat(Math.ceil(message.length / key.length));
+    let keyIndex = 0;
+    for (let i = 0; i < message.length; i++) {
+      if (message.charCodeAt(i) > 64 && message.charCodeAt(i) < 91) {
+        let newCharCode = ((message.charCodeAt(i) - key.charCodeAt(keyIndex) + 26) % 26) + 65;
+        result += String.fromCharCode(newCharCode);
+        keyIndex++;
+      }
+      else {
+        result += message[i];
+      }
+    }
+    if (this.reverse) {
+      result = result.split('').reverse().join('');
+    }
+    return result;
   }
 }
 
